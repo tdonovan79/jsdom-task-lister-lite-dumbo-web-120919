8f4d8cf86b25f95e3563e6bf8a7978d33802c510
tdonovan79 <tdonovan79@gmail.com>
2020-01-28 18:43:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submitForm = document.querySelector("#create-task-form");
+  
+
+  //get input forma nd save to event listener
+  submitForm.addEventListener("submit", handleSubmitForm);
+  
+  //handle submit form, add task typed in to list of MyTodos
+  function handleSubmitForm(e){
+    event.preventDefault()
+    const inputForm = e.target;
+    // debugger
+    document.querySelector("#list").append(inputForm["new-task-description"].value);
+    document.querySelector("#list").append(document.createElement("br"));
+
+  };
 });
